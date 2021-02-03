1ec93c92dccd3ec95f02917912dfdafbf291de0a
Angela Engel <amengel.ae@icloud.com>
2021-02-02 19:53:10 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./Components/Header"
+import PhotoOfDay from "./Components/PhotoOfDay"
+import Footer from "./Components/Footer"
+
 
 function App() {
+  const [ nasaData, setNasaData ] = useState([])
+
+  const fetchPhoto = () => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        console.log(response)
+        setNasaData(response.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(fetchPhoto, [])
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      
+       {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>!*/}
+        <Header nasaData={nasaData}/>
+        <PhotoOfDay nasaData={nasaData}/>
+        <Footer nasaData={nasaData}/>
+      
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+// ICE
+import React from "react";
+
+export default function Header (props) {
+    const { nasaData } = props
+
+    return (
+        <div>
+            <h1>Nasa Photo of the Day</h1>
+            <p>{nasaData.date}</p>
+            <p>{nasaData.title}</p>
+            <p>{nasaData.explantion}</p>
+        </div>
+    )
+}
